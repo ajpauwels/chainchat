f13f370a1c01e3ca22dc18b5fa71301467142038
Alexandre Pauwels <apauwel@us.ibm.com>
2016-06-09 13:28:53 -0400

    Completed UI, integrated socket.io server but implementation remains to be done

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var io = require('socket.io')();
+app.io = io;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+io.on('connection', function(socket) {
+  console.log('A user connected to the socket.io server');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
